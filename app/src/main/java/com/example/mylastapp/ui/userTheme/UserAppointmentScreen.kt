0b59db50d5288eae8190fbb8093b90f28ce648b4
weiqi629 <tangwq-wm24@student.tarc.edu.my>
2025-09-19 02:33:34 +0800

    UserAppointmentScreen

--- app/src/main/java/com/example/mylastapp/ui/userTheme/UserAppointmentScreen.kt
+++ app/src/main/java/com/example/mylastapp/ui/userTheme/UserAppointmentScreen.kt
@@ -296,7 +296,7 @@ fun UserAppointmentScreen(
                             modifier = Modifier.padding(bottom = 12.dp)
                         )
                     }
-                    Spacer(modifier = Modifier.height(60.dp))
+                    Spacer(modifier = Modifier.height(26.dp))
 
                     if (totalDoctorPages > 1) {
                         PageBar(
